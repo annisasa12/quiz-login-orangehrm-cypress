edc5de7d8d7c21711e0eb8a21b4a41dbfde63461
annisasa12 <flowerannisa12@gmail.com>
2026-07-18 23:41:25 +0700

    Create testData.js

---
+++ cypress/e2e/final-project/data/testData.js
@@ -0,0 +1,21 @@
+const testData = {
+  baseUrl:
+    'https://opensource-demo.orangehrmlive.com/web/index.php',
+
+  login: {
+    validUsername: 'Admin',
+    validPassword: 'admin123',
+    invalidUsername: 'adminSalah',
+    invalidPassword: 'passwordSalah',
+  },
+
+  directory: {
+    employeeName: 'a',
+  },
+
+  recruitment: {
+    candidateName: 'a',
+  },
+}
+
+export default testData
